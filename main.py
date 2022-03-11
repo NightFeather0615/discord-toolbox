db400b470b283d99ff701a2cffd03f072bdad77e
NightFeather <leolee50910@gmail.com>
2022-03-12 01:14:05 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,45 +1,62 @@
 import requests
 import time
-import logging
+import os, sys
 
-user_token = input("User Token (mfa): ")
-channel_id = input("Channel ID: ")
-folder_path = input("Folder: ")
-
-logging.basicConfig(level = logging.INFO,
-                    format = '[%(asctime)s] [%(levelname)s] %(message)s',
-                    datefmt = '%Y/%m/%d %I:%M:%S')
+user_token = input("[User Token (not Bearer token)]\n$ ")
+channel_id = input("[Channel ID]\n$ ")
 
 def main():
-  data = requests.get(f"https://discord.com/api/channels/{channel_id}/messages", headers = {"Authorization": user_token})
-  data = data.json()
-  for message in data:
-    logging.info(f"Message: {message['id']} | Timestamp: {message['timestamp']} | Attachments: {len(message['attachments'])}")
-    for attachment in message["attachments"]:
-      if attachment["content_type"].startswith("image/"):
-        file_name = hash(message["id"] + message["timestamp"] + attachment["url"])
-        with open(f"./{folder_path}/{file_name}.png", "wb") as f:
-          f.write(requests.get(attachment["url"]).content)
-  logging.info("Sleep for 30 seconds to avoid rate limit...")
-  time.sleep(30)
-  continue_fetch_images(data[-1]["id"])
+  fetch_user(user_token)
 
-def continue_fetch_images(before: str):
-  data = requests.get(f"https://discord.com/api/channels/{channel_id}/messages?before={before}", headers = {"Authorization": user_token})
+def fetch_user(user_token: str):
+  os.system('cls' if os.name == 'nt' else 'clear')
+  url = "https://discord.com/api/users/@me"
+  user_data = requests.get(url, headers = {"Authorization": user_token})
+  user_data = user_data.json()
+  print(f"[Logged in as user]\n - ID: {user_data['id']}\n - Username: {user_data['username']}\n - Discriminator: {user_data['discriminator']}")
+  check_is_continue()
+
+def check_is_continue():
+  print("\n[Warning]\n - This program use self-bot to fetch image, which aginest Discord ToS, are you sure you want to continue? (y/n)")
+  while True:
+    choice = input(f"$ ")
+    if choice.lower() == "y":
+      try:
+        os.mkdir(f"./Fetched-Channels/{channel_id}")
+      except FileExistsError:
+        pass
+      fetch_images(channel_id)
+      break
+    elif choice.lower() == "n":
+      sys.exit()
+    else:
+      print("[Error]\n - Invalid input")
+
+def fetch_images(channel_id: str, before: str = None):
+  url = "https://discord.com/api/channels/" + (f"{channel_id}/messages" if before is None else f"{channel_id}/messages?before={before}")
+  data = requests.get(url, headers = {"Authorization": user_token})
   data = data.json()
-  for message in data:
-    logging.info(f"Message: {message['id']} | Timestamp: {message['timestamp']} | Attachments: {len(message['attachments'])}")
-    for attachment in message["attachments"]:
-      if attachment["content_type"].startswith("image/"):
-        file_name = hash(message["id"] + message["timestamp"] + attachment["url"])
-        with open(f"./{folder_path}/{file_name}.png", "wb") as f:
-          f.write(requests.get(attachment["url"]).content)
-  logging.info("Sleep for 30 seconds to avoid rate limit...")
-  time.sleep(30)
+  download_image(data)
   if len(data) == 50:
-    continue_fetch_images(data[-1]["id"])
+    fetch_images(channel_id, data[-1]["id"])
   else:
-    logging.info("No more image to fetch.")
+    print("\n[Program exits]\n - No more messages content attachments to fetch.")
+    sys.exit()
+
+def download_image(data):
+  for message in data:
+    if message["attachments"] != []:
+      print("")
+      print(f"[Fetched message]\n - ID: {message['id']}\n - Attachments: {len(message['attachments'])}")
+      for attachment in message["attachments"]:
+        if attachment["content_type"].startswith("image/"):
+          print("")
+          file_name = str(hash(message["id"] + message["timestamp"] + attachment["url"])).replace("-", "")
+          with open(f"./Fetched-Channels/{channel_id}/{file_name}.png", "wb") as f:
+            raw_image = requests.get(attachment["url"])
+            file_size = round(raw_image.content.__sizeof__() * 0.001, 1)
+            print(f"[Download Image]\n - Name: {file_name}.png\nSize: ~{file_size} KB")
+            f.write(raw_image.content)
 
 if "__main__" == __name__:
   main()
