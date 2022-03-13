9e9c20ac99b7a803bfc80ee8d7034bf6874a0e7c
NightFeather <leolee50910@gmail.com>
2022-03-14 01:33:25 +0800

    Rewrite with argparse

--- main.py
+++ main.py
@@ -1,65 +1,88 @@
 import requests
-import time
+import argparse
 import os, sys
 
-user_token = input("[User Token (not Bearer token)]\n$ ")
-channel_id = input("[Channel ID]\n$ ")
+class Core(object):
+  def __init__(self):
+    pass
 
-def main():
-  fetch_user(user_token)
+  def make_folder(channel_id):
+    if not os.path.exists(f"./{channel_id}"):
+      os.mkdir(f"./{channel_id}")
 
-def fetch_user(user_token: str):
-  os.system('cls' if os.name == 'nt' else 'clear')
-  url = "https://discord.com/api/users/@me"
-  user_data = requests.get(url, headers = {"Authorization": user_token})
-  user_data = user_data.json()
-  if user_data.get('code') == 0:
-    print("[Error]\n - Invalid token.")
-    sys.exit()
-  print(f"[Logged in as user]\n - ID: {user_data['id']}\n - Username: {user_data['username']}\n - Discriminator: {user_data['discriminator']}")
-  check_is_continue()
+  def build_parser():
+    parser = argparse.ArgumentParser(description = 'Discord self-bot tools.', epilog = 'Made by NightFeather')
 
-def check_is_continue():
-  print("\n[Warning]\n - This program use self-bot to fetch image, which aginest Discord ToS, are you sure you want to continue? (y/n)")
-  while True:
-    choice = input(f"$ ")
-    if choice.lower() == "y":
+    tool_parsers = parser.add_subparsers(help='Tools', dest="tool")
+    tool_parsers.required = True
+
+    fetch_images_parser = tool_parsers.add_parser('fetch-images', help='fetch images from channel (only available if you are able to access the channel)')
+    fetch_images_parser.add_argument('--token', "-t", required=True, nargs=1, type=str, help='user token (not bearer token)')
+    fetch_images_parser.add_argument('--channel-id', "-c", required=True, nargs=1, type=str, help='discord channel ID')
+    fetch_images_parser.add_argument('--output-dir', "-o", required=False, nargs=1, type=str, help='output directory')
+    fetch_images_parser.add_argument('--quiet', "-q", required=False, action='store_true', help='quiet mode')
+    fetch_images_parser.add_argument('--version', "-v", action='version', version='%(prog)s 0.1')
+
+    return parser.parse_args()
+
+class Discord(object):
+  def __init__(self):
+    pass
+
+  def fetch_user(token: str):
+    user_data = requests.get("https://discord.com/api/users/@me", headers = {"Authorization": token}).json()
+    if user_data.get('code') == 0:
+      print("HTTP Error: 401 Unauthorized, invalid token")
+      return None
+    else:
+      print(f"\n[Login success]\nUser: {user_data['username']}#{user_data['discriminator']}\nID: {user_data['id']}\n")
+      return user_data
+
+class ImageFetcher(object):
+  def __init__(self):
+    pass
+
+  def setup(token):
+    user = Discord.fetch_user(token)
+    if user != None:
+      pass
+
+
+  def fetch_images(channel_id: str, token: str, output_dir: str, quiet: bool, before: str = None):
+    if output_dir == None:
       try:
-        os.mkdir(f"./Fetched-Channels/{channel_id}")
+        output_dir = f"./{channel_id}"
+        os.makedirs(output_dir)
       except FileExistsError:
         pass
-      fetch_images(channel_id)
-      break
-    elif choice.lower() == "n":
-      sys.exit()
+    url = f"https://discord.com/api/channels/{channel_id}/messages" + ("" if before is None else f"?before={before}")
+    raw_messages = requests.get(url, headers = {"Authorization": token})
+    messages = raw_messages.json()
+    ImageFetcher.download_image(messages, output_dir, quiet)
+    if len(messages) == 50:
+      ImageFetcher.fetch_images(channel_id, token, output_dir, quiet, messages[-1]["id"])
     else:
-      print("[Error]\n - Invalid input")
+      print("Fetching complete.")
 
-def fetch_images(channel_id: str, before: str = None):
-  url = "https://discord.com/api/channels/" + (f"{channel_id}/messages" if before is None else f"{channel_id}/messages?before={before}")
-  data = requests.get(url, headers = {"Authorization": user_token})
-  data = data.json()
-  download_image(data)
-  if len(data) == 50:
-    fetch_images(channel_id, data[-1]["id"])
-  else:
-    print("\n[Program exits]\n - No more messages content attachments to fetch.")
-    sys.exit()
+  def download_image(messages, output_dir: str, quiet: bool):
+    for message in messages:
+      if message["attachments"] != []:
+        for attachment in message["attachments"]:
+          if attachment["content_type"].startswith("image/"):
+            file_name = str(hash(message["id"] + message["timestamp"] + attachment["url"])).replace("-", "")
+            with open(f"{output_dir}/{file_name}.png", "wb") as f:
+              image = requests.get(attachment["url"]).content
+              f.write(image)
+            if quiet == False:
+              print(f"Downloaded {file_name}.png, ~{round(len(image) * 0.001, 1)} KB.")
 
-def download_image(data):
-  for message in data:
-    if message["attachments"] != []:
-      print("")
-      print(f"[Fetched message]\n - ID: {message['id']}\n - Attachments: {len(message['attachments'])}")
-      for attachment in message["attachments"]:
-        if attachment["content_type"].startswith("image/"):
-          print("")
-          file_name = str(hash(message["id"] + message["timestamp"] + attachment["url"])).replace("-", "")
-          with open(f"./Fetched-Channels/{channel_id}/{file_name}.png", "wb") as f:
-            raw_image = requests.get(attachment["url"])
-            file_size = round(raw_image.content.__sizeof__() * 0.001, 1)
-            print(f"[Download Image]\n - Name: {file_name}.png\nSize: ~{file_size} KB")
-            f.write(raw_image.content)
+def main():
+  args = Core.build_parser()
+  print(args)
+  if Discord.fetch_user(args.token[0]) != None:
+    print("Starting...\n")
+    if args.tool == "fetch-images":
+      ImageFetcher.fetch_images(args.channel_id[0], args.token[0], args.output_dir, args.quiet)
 
 if "__main__" == __name__:
   main()
