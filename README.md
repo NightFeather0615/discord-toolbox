b0de5037dc2c43523c873c49545a8fbfccb151b7
NightFeather <leolee50910@gmail.com>
2022-03-15 03:12:30 +0800

    New feature
    [+] Change HypeSquad House
    [+] Check Token
    [~] Command format
    [~] README.md

--- README.md
+++ README.md
@@ -2,13 +2,44 @@
 a simple Discord self-bot tools combination package
 
 # Available tools
-* backup-images
-* fetch-emojis
-* fetch-stickers
+* Check Token
+* Change HypeSquad House
+* Backup Images
+* Fetch Emojis
+* Fetch Stickers
 
-## backup-images
+## Basic Command format
+`python3 main.py --token TOKEN [TOOL] [Args]`
+
+E.g.   
+`python3 main.py --token OTIASDEQDA5Mjky.YEWQEQmw.PlM2JJQWEQWEDJ7HDyhrw change-hypesquad --house-id 1`
+
+## Check Token
 Quick example:  
-`python3 main.py backup-images --channel-id 456574854780912354 --token OTIASDEQDA5Mjky.YEWQEQmw.PlM2JJQWEQWEDJ7HDyhrw`
+`python3 main.py --token OTIASDEQDA5Mjky.YEWQEQmw.PlM2JJQWEQWEDJ7HDyhrw`
+
+|Argument|Alias|Required|Description|
+|-|-|-|-|
+|--token|-t|True|Discord user token (not bearer token)|
+
+## Change HypeSquad House
+Quick example:  
+`python3 main.py --token OTIASDEQDA5Mjky.YEWQEQmw.PlM2JJQWEQWEDJ7HDyhrw change-hypesquad --house-id 1`
+
+|Argument|Alias|Required|Description|
+|-|-|-|-|
+|--token|-t|True|Discord user token (not bearer token)|
+|--house-id|-c|True|HypeSquad house ID|
+
+|ID|House|
+|-|-|
+|1|Bravery|
+|2|Brilliance|
+|3|Balance|
+
+## Backup Images
+Quick example:  
+`python3 main.py --token OTIASDEQDA5Mjky.YEWQEQmw.PlM2JJQWEQWEDJ7HDyhrw backup-images --channel-id 456574854780912354`
 
 |Argument|Alias|Required|Description|
 |-|-|-|-|
@@ -17,9 +48,9 @@ Quick example:
 |--output-dir|-o|False|Output directory|
 |--quiet|-q|False|Quiet output|
 
-## fetch-emojis
+## Fetch Emojis
 Quick example:  
-`python3 main.py fetch-emojis --guild-id 456574854780912354 --token OTIASDEQDA5Mjky.YEWQEQmw.PlM2JJQWEQWEDJ7HDyhrw`
+`python3 main.py --token OTIASDEQDA5Mjky.YEWQEQmw.PlM2JJQWEQWEDJ7HDyhrw fetch-emojis --guild-id 456574854780912354`
 
 |Argument|Alias|Required|Description|
 |-|-|-|-|
@@ -28,9 +59,9 @@ Quick example:
 |--output-dir|-o|False|Output directory|
 |--quiet|-q|False|Quiet output|
 
-## fetch-stickers
+## Fetch Stickers
 Quick example:  
-`python3 main.py fetch-stickers --guild-id 456574854780912354 --token OTIASDEQDA5Mjky.YEWQEQmw.PlM2JJQWEQWEDJ7HDyhrw`
+`python3 main.py --token OTIASDEQDA5Mjky.YEWQEQmw.PlM2JJQWEQWEDJ7HDyhrw fetch-stickers --guild-id 456574854780912354`
 
 |Argument|Alias|Required|Description|
 |-|-|-|-|
--- main.py
+++ main.py
@@ -2,17 +2,19 @@ import requests
 import argparse
 import os, sys
 import apnggif
+import typing
+import random
 
 
 class Core(object):
   def __init__(self):
     pass
 
-  def make_folder(channel_id):
+  def make_folder(channel_id) -> None:
     if not os.path.exists(f"./{channel_id}"):
       os.mkdir(f"./{channel_id}")
 
-  def download_image(url: str, file_name: str, file_type: str, output_dir: str, quiet: bool):
+  def download_image(url: str, file_name: str, file_type: str, output_dir: str, quiet: bool) -> None:
     with open(f"{output_dir}/{file_name}.{file_type}", "wb") as f:
       image = requests.get(url).content
       f.write(image)
@@ -21,24 +23,25 @@ class Core(object):
 
   def build_parser():
     parser = argparse.ArgumentParser(description = 'Discord self-bot tools.', epilog = 'Made by NightFeather')
+    parser.add_argument('--token', "-t", required=True, nargs=1, type=str, help='user token (not bearer token)')
 
-    tool_parsers = parser.add_subparsers(dest="tool", title = 'Tools', required = True)
+    tool_parsers = parser.add_subparsers(dest="tool", title = 'Tools')
+
+    change_hypesquad_parser = tool_parsers.add_parser('change-hypesquad', help='change hypesquad house directly without filling out the form')
+    change_hypesquad_parser.add_argument('--house-id', "-i", required=True, choices=[1, 2, 3], nargs=1, type=int, help='hypersquad guild id [1: Bravery, 2: Brilliance, 3: Balance]')
 
     fetch_images_parser = tool_parsers.add_parser('backup-images', help='backup images from channel (only available if you are able to access the channel)')
-    fetch_images_parser.add_argument('--token', "-t", required=True, nargs=1, type=str, help='user token (not bearer token)')
-    fetch_images_parser.add_argument('--channel-id', "-c", required=True, nargs=1, type=str, help='discord channel ID')
+    fetch_images_parser.add_argument('--channel-id', "-c", required=True, nargs=1, type=str, help='discord channel id')
     fetch_images_parser.add_argument('--output-dir', "-o", required=False, nargs=1, type=str, help='output directory')
     fetch_images_parser.add_argument('--quiet', "-q", required=False, action='store_true', help='quiet mode')
 
     fetch_emojis_parser = tool_parsers.add_parser('fetch-emojis', help='fetch emojis from guild (only available if you are able to access the guild)')
-    fetch_emojis_parser.add_argument('--token', "-t", required=True, nargs=1, type=str, help='user token (not bearer token)')
-    fetch_emojis_parser.add_argument('--guild-id', "-g", required=True, nargs=1, type=str, help='discord guild ID')
+    fetch_emojis_parser.add_argument('--guild-id', "-g", required=True, nargs=1, type=str, help='discord guild id')
     fetch_emojis_parser.add_argument('--output-dir', "-o", required=False, nargs=1, type=str, help='output directory')
     fetch_emojis_parser.add_argument('--quiet', "-q", required=False, action='store_true', help='quiet mode')
 
     fetch_stickers_parser = tool_parsers.add_parser('fetch-stickers', help='fetch stickers from guild (only available if you are able to access the guild)')
-    fetch_stickers_parser.add_argument('--token', "-t", required=True, nargs=1, type=str, help='user token (not bearer token)')
-    fetch_stickers_parser.add_argument('--guild-id', "-g", required=True, nargs=1, type=str, help='discord guild ID')
+    fetch_stickers_parser.add_argument('--guild-id', "-g", required=True, nargs=1, type=str, help='discord guild id')
     fetch_stickers_parser.add_argument('--output-dir', "-o", required=False, nargs=1, type=str, help='output directory')
     fetch_stickers_parser.add_argument('--quiet', "-q", required=False, action='store_true', help='quiet mode')
 
@@ -48,20 +51,38 @@ class Discord(object):
   def __init__(self):
     pass
 
-  def fetch_user(token: str):
+  def fetch_user(token: str) -> dict:
     user_data = requests.get("https://discord.com/api/users/@me", headers = {"Authorization": token}).json()
     if user_data.get('code') == 0:
-      print("HTTP Error: 401 Unauthorized, invalid token")
+      print("\n[Login failed]\nHTTP Error: 401 Unauthorized, invalid token")
       return None
     else:
-      print(f"\nLogin success\nUser: {user_data['username']}#{user_data['discriminator']}\nID: {user_data['id']}\n")
+      user_info = f"\n[Login success]\nUser: {user_data['username']}#{user_data['discriminator']}\nID: {user_data['id']}\nEmail: {user_data['email']}\nLocale: {user_data['locale']}\nMFA: {user_data['mfa_enabled']}\n"
+      print(user_info)
       return user_data
+
+  def change_hypesquad_house(token: str, house: typing.Union[str, int] = None) -> None:
+    url = "https://discord.com/api/hypesquad/online"
+    house = int(house)
+    if house == None:
+      house = random.randint(1, 3)
+    id_to_name = {
+      1: "Bravery",
+      2: "Brilliance",
+      3: "Balance"
+    }
+    payload = {'house_id': house}
+    respond = requests.post(url, headers = {"Authorization": token, "Content-Type": "application/json"}, json = payload)
+    if respond.status_code == 204:
+      print(f"Hypesquad house changed to {id_to_name[house]}")
+    else:
+      print(f"Hypesquad house change failed, HTTP Error: {respond.status_code}")
 
 class ImageFetcher(object):
   def __init__(self):
     pass
 
-  def fetch_images(channel_id: str, token: str, output_dir: str, quiet: bool, before: str = None):
+  def fetch_images(channel_id: str, token: str, output_dir: str, quiet: bool, before: str = None) -> None:
     if output_dir == None:
       output_dir = f"./backups/{channel_id}/images"
       Core.make_folder("./backups")
@@ -88,7 +109,7 @@ class EmojisFetcher(object):
   def __init__(self):
     pass
 
-  def fetch_emojis(guild_id: str, token: str, output_dir: str, quiet: bool):
+  def fetch_emojis(guild_id: str, token: str, output_dir: str, quiet: bool) -> None:
     if output_dir == None:
       output_dir = f"./emojis/{guild_id}"
       Core.make_folder("./emojis")
@@ -108,7 +129,7 @@ class StickersFetcher(object):
   def __init__(self):
     pass
 
-  def fetch_stickers(guild_id: str, token: str, output_dir: str, quiet: bool):
+  def fetch_stickers(guild_id: str, token: str, output_dir: str, quiet: bool) -> None:
     if output_dir == None:
       output_dir = f"./stickers/{guild_id}"
       Core.make_folder("./stickers")
@@ -129,13 +150,16 @@ class StickersFetcher(object):
 def main():
   args = Core.build_parser()
   if Discord.fetch_user(args.token[0]) != None:
-    print("Starting...\n")
-    if args.tool == "backup-images":
-      ImageFetcher.fetch_images(args.channel_id[0], args.token[0], args.output_dir, args.quiet)
-    elif args.tool == "fetch-stickers":
-      StickersFetcher.fetch_stickers(args.guild_id[0], args.token[0], args.output_dir, args.quiet)
-    elif args.tool == "fetch-emojis":
-      EmojisFetcher.fetch_emojis(args.guild_id[0], args.token[0], args.output_dir, args.quiet)
+    if args.tool != None:
+      print("Starting...\n")
+      if args.tool == 'change-hypesquad':
+        Discord.change_hypesquad_house(args.token[0], args.house_id[0])
+      elif args.tool == "backup-images":
+        ImageFetcher.fetch_images(args.channel_id[0], args.token[0], args.output_dir, args.quiet)
+      elif args.tool == "fetch-stickers":
+        StickersFetcher.fetch_stickers(args.guild_id[0], args.token[0], args.output_dir, args.quiet)
+      elif args.tool == "fetch-emojis":
+        EmojisFetcher.fetch_emojis(args.guild_id[0], args.token[0], args.output_dir, args.quiet)
 
 if "__main__" == __name__:
   main()
